422d7decf15b9b74b93bdc75af47e3d16d6323ff
keirruttley <Keir.Ruttley-Dornan@student.norwichuni.ac.uk>
2024-10-10 15:58:01 +0100

    change image upon button click

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const myButton = document.getElementById("btn");
 const myParagraph = document.getElementById("my-paragraph");
+const characterImage = document.getElementById("character-image")
 
 let clickCounter = 0;
 
 function clickFunction() {
-
     console.log("the button has been clicked");
     clickCounter = clickCounter + 3;
+
+    characterImage.src = "assets/arbiter.webp"
 
     myParagraph.innerHTML = "I have been clicked " + clickCounter + " time(s)";
 }
 
 myButton.onclick = clickFunction;
+
